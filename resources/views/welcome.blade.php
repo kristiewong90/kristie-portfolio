79919cdc75916caea0d21c4fb45016e7598bcb11
Kristie Wong <kristie.wong90@gmail.com>
2018-04-09 17:30:15 -0600

    first commit - WIP navbar

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Kristie Wong | Web Developer</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="/css/app.css?<?php echo time();?>">
+
+        <!-- Styles -->
+      <body>
+        <nav class="navbar navbar-default navbar-expand-lg fixed-top">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">X</span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item"><a class="link-item" href="/">Icon</a></li>
+            </ul>
+            <ul class="navbar-nav navbar-right">
+              <li class="nav-item active">
+                <a class="link-item" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="link-item" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="link-item" href="#">Portfolio</a>
+              </li>
+              <li class="nav-item">
+                <a class="link-item" href="#">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="link-item" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+
+        <!-- <nav class="navbar navbar-default navbar-expand-lg fixed-top">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item right"><a class="link-item" href="/">Home</a></li>
+          </ul>
+          <ul class="navbar-nav">
+            <li class="nav-item left"><a class="link-item" href="/About">About</a></li>
+            <li class="nav-item left"><a class="link-item" href="/Portfolio">Portfolio</a></li>
+            <li class="nav-item left"><a class="link-item" href="/Blog">Blog</a></li>
+            <li class="nav-item left"><a class="link-item" href="/Contact">Contact</a></li>
+          </ul>
+        </nav> -->
+
+        <div class="hero row">
+          <div class="designer col-sm">
+            <h1 class=bounce-left>designer</h1>
+          </div>
+          <div class="developer col-sm">
+            <h1 class="bounce-right">developer</h1>
+          </div>
+        </div>
+
+      </body>
+
+</html>
