c660a0b8d868acc11fa2462f770a0354e779b647
Kristie Wong <kristie.wong90@gmail.com>
2018-09-26 21:19:03 -0600

    Updated about me description

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,7 +10,7 @@
       <div class="row">
         <h1 class="col-7">About me</h1>
 
-        <p class="col-7 description">My name is Kriste. I like to design websites. My background is a bit unusal. I studied Economics & Business at the University of Victoria.
+        <p class="col-7 description">Hi! I'm kristie, I like designing websites. My background is a bit unusal. I studied Economics & Business at the University of Victoria.
           I also have my certificate in Web Development. I'm currenly working as a UX / UI Designer for Dissolve Inc. which is based in Calgary.
           {{-- I also like to learn new skills. I'm currently trying to learn more about photography. If anyone has any pointers for a beginner send me an email! --}}
         </p>
