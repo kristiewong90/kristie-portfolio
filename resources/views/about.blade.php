99bc51bfb13f57fce95c75a982e31bfbb375f38c
Kristie Wong <kristie.wong90@gmail.com>
2018-09-26 22:18:56 -0600

    WIP mobile responsiveness for welcome blade, about blade, & contact blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,9 +8,9 @@
 
     <div class="content container">
       <div class="row">
-        <h1 class="col-7">About me</h1>
+        <h1 class="col-8">About me</h1>
 
-        <p class="col-7 description">Hi! I'm kristie, I like designing websites. My background is a bit unusal. I studied Economics & Business at the University of Victoria.
+        <p class="col-8 description">Hi! I'm kristie, I like designing websites. My background is a bit unusal. I studied Economics & Business at the University of Victoria.
           I also have my certificate in Web Development. I'm currenly working as a UX / UI Designer for Dissolve Inc. which is based in Calgary.
           {{-- I also like to learn new skills. I'm currently trying to learn more about photography. If anyone has any pointers for a beginner send me an email! --}}
         </p>
